346bb7b248cd4d391aa4b7a2c0edead5d3aa5c9a
Erika Pauwels <erika.pauwels@gmail.com>
2018-05-11 16:05:54 +0200

    Add Ember Data models

---
+++ app/models/form-input.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default DS.Model.extend({
+  index: attr(),
+  displayType: attr(),
+  options: attr('json'),
+  identifier: attr(),
+  parent: belongsTo('form-node'),
+  dynamicSubforms: hasMany('dynamic-subform')
+});
